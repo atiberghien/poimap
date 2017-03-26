83497911c07bdbff89a3e7eb4564b9cb78d75c0e
Pierre <pierre.sarrus@gmail.com>
2017-03-26 22:50:13 +0200

    create form your itineray

--- urls.py
+++ urls.py
@@ -6,9 +6,9 @@ from . import views
 from views import *
 
 urlpatterns = [
-    # url(r'^create/$', HostingCreateView.as_view(), name='hosting_create'),
+    url(r'^create/$', views.test, name='itinerary_create'),
     # url(r'^detail/(?P<pk>[0-9]+)/$', HostingDetailView.as_view(), name='hosting_detail'),
     # url(r'^list/$', HostingListView.as_view(), name='hosting_list'),
     # url(r'^map/$', HostingMapView.as_view(), name='hosting_map'),
 
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- views.py
+++ views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Base
 
 # Create your views here.
+
+def test(request):
+    bases = Base.objects.all()
+    return render(request, 'itinerary.html',{'bases': bases})
