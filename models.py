0e77cae4967ba0c2e2cc7206a6e8f4fadb6d6bb2
Alban atiberghien Tiberghien <alban.tiberghien@gmail.com>
2017-03-28 21:01:57 +0200

    Alias import

--- models.py
+++ models.py
@@ -1,14 +1,13 @@
 from django.db import models
-from django.contrib.gis.db import models
-from django.contrib.gis.db.models import PointField
 from filer.models import File
+from django.contrib.gis.db import models as gismodels
 from filer.fields.image import FilerImageField
 
 
 # Create your models here.
 class Base(models.Model):
     name = models.CharField(max_length=50)         # nom du point de depart - Il pourrait aussi etre un hebergement
-    geo_coordinates = models.PointField(null=True, blank=True)      # coordonnees du point de depart
+    geo_coordinates = gismodels.PointField(null=True, blank=True)      # coordonnees du point de depart
     description = models.TextField(blank=True)
     media = FilerImageField(null=True, blank=True,)
 
