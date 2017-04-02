3adc3af5a6e249626ca536bf6851d93205cc0719
Alban atiberghien Tiberghien <alban.tiberghien@gmail.com>
2017-04-02 14:02:11 +0200

    Remove unused url/view

--- urls.py
+++ urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import url
-from .views import test, POIMapView
+from .views import POIMapView
 
 urlpatterns = [
-    url(r'^create/$', test, name='itinerary_create'),
     url(r'^map/$', POIMapView.as_view(), name='poi_map'),
 ]
--- views.py
+++ views.py
@@ -3,11 +3,6 @@ from django.views.generic import ListView
 from .models import Base, POI
 
 
-def test(request):
-    bases = Base.objects.all()
-    return render(request, 'itinerary.html',{'bases': bases})
-
-
 class POIMapView(ListView):
     model = POI
     template_name = "poi_map.html"
