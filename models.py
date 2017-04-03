fab56a1c2a73b206cc854f24f8c081549a0f6b37
Alban atiberghien Tiberghien <alban.tiberghien@gmail.com>
2017-04-03 18:05:10 +0200

    Increase POI name size

--- models.py
+++ models.py
@@ -25,8 +25,8 @@ class POIType(models.Model):  # POI = Point Of Interest
 
 
 class POI(PolymorphicModel):
-    name = models.CharField(max_length=50)         # nom du point de depart - Il pourrait aussi etre un hebergement
     description = models.TextField(blank=True)
+    name = models.CharField(max_length=500)         # nom du point de depart - Il pourrait aussi etre un hebergement
     type = models.ForeignKey(POIType)
 
     @property
