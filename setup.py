cf124e08745cbf1230f36f5729f15ba9234fd3d5
Alban atiberghien Tiberghien <alban.tiberghien@gmail.com>
2017-05-16 14:18:43 +0200

    Build python package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='poimap',
+      version='0.1',
+      description='Set POI on a map',
+      url='https://github.com/atiberghien/poimap',
+      author='Alban Tiberghien',
+      author_email='alban.tiberghien@gmail.com',
+      license='MIT',
+      packages=['poimap'],
+      zip_safe=False)
