3611fc212ebcfd2baffaa376ae4054f639fe7cca
Alban Tiberghien <alban.tiberghien@gmail.com>
2017-07-17 18:30:09 +0200

    Add python deps

--- setup.py
+++ setup.py
@@ -8,4 +8,15 @@ setup(name='poimap',
       author_email='alban.tiberghien@gmail.com',
       license='MIT',
       packages=['poimap'],
+      install_requires=[
+           'Django>=1.11.3'
+           'django_countries>=4.6.1',
+           'djangorestframework>=3.6.3',
+           'djangorestframework-gis',
+           'django-treebeard>=4.1.2',
+           'django-polymorphic>=1.2',
+           'django-polymorphic-tree',
+           'django-filer>=1.2.7',
+           'easy-thumbnails>=2.4.1'
+      ],
       zip_safe=False)
