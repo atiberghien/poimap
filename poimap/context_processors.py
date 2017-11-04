3efb4cfdc89fdef911af38b1db7987e9bdff4b1d
Alban Tiberghien <alban.tiberghien@gmail.com>
2017-11-04 17:34:14 +0100

    Add POI_CUSTOM_ITINERARY in template context

--- poimap/context_processors.py
+++ poimap/context_processors.py
@@ -2,6 +2,7 @@ from django.conf import settings
 
 def poimap(request):
     context = {
-        "POI_UNDER_CONTROL" : getattr(settings, "POI_UNDER_CONTROL", True)
+        "POI_UNDER_CONTROL" : getattr(settings, "POI_UNDER_CONTROL", True),
+        "POI_CUSTOM_ITINERARY" : getattr(settings, "POI_CUSTOM_ITINERARY", True)
     }
     return context
