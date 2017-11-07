2baaed1fd83a0c78a4f6d135a00915afc8104c0e
Alban Tiberghien <alban.tiberghien@gmail.com>
2017-11-07 13:49:18 +0100

    Slip api et standard urls

---
+++ poimap/api_urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import url
+from .views import *
+
+urlpatterns = [
+    url(r'^area/(?P<slug>[\w-]+)/$', AreaView.as_view(), name='api-area'),
+    url(r'^area/(?P<slug>[\w-]+)/paths/$', AreaPathsView.as_view(), name='api-area-paths'),
+    url(r'^area/(?P<slug>[\w-]+)/subpaths/$', SubPathListView.as_view(), name='api-subpath-list'),
+
+    url(r'^path/(?P<slug>[\w-]+)/$', PathView.as_view(), name='path-api-detail'),
+
+    url(r'^poi/(?P<pk>\d+)/$', POIView.as_view(), name='poi-api-detail'),
+
+    url(r'^path/(?P<path_pk>\d+)/poi/list/$', POIList.as_view(), name='api-poi-list'),
+    url(r'^poi/list/$', POIList.as_view(), name='api-poi-list'),
+]
--- poimap/urls.py
+++ poimap/urls.py
@@ -2,20 +2,9 @@ from django.conf.urls import url
 from .views import *
 
 urlpatterns = [
-
-    url(r'^api/area/(?P<slug>[\w-]+)/$', AreaView.as_view(), name='api-area'),
-    url(r'^api/area/(?P<slug>[\w-]+)/paths/$', AreaPathsView.as_view(), name='api-area-paths'),
-    url(r'^api/area/(?P<slug>[\w-]+)/subpaths/$', SubPathListView.as_view(), name='api-subpath-list'),
-
-    url(r'^api/path/(?P<slug>[\w-]+)/$', PathView.as_view(), name='path-api-detail'),
-
-    url(r'^api/poi/(?P<pk>\d+)/$', POIView.as_view(), name='poi-api-detail'),
-
-    url(r'^api/path/(?P<path_pk>\d+)/poi/list/$', POIList.as_view(), name='api-poi-list'),
-    url(r'^api/poi/list/$', POIList.as_view(), name='api-poi-list'),
-
-
     url(r'^poi/(?P<pk>\d+)/convert/(?P<to_model>\w+)$', convert_poi, name='convert-poi'),
+    url(r'^poi/(?P<slug>[\w-]+)/$', POIDetailView.as_view(), name='poi-detail'),
     url(r'^(?P<area_slug>[\w-]+)/$', MapView.as_view(), name='map'),
-    url(r'^(?P<path_slug>[\w-]+)/itinerary/$', custom_itinerary, name='itinerary')
+    url(r'^(?P<path_slug>[\w-]+)/itinerary/$', custom_itinerary, name='itinerary'),
+    url(r'^$', MapView.as_view(), name='map-default'),
 ]
