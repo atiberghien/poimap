9edf024b0e42779467e3a7b33e03501845fb2872
Alban Tiberghien <alban.tiberghien@gmail.com>
2018-01-10 14:45:57 +0100

    Remove useless import

--- admin.py
+++ admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from django.contrib.gis import admin
 from models import Hostings, PaymentType, OpeningDate, Contact, SleepingType, Sleeping
 
 from leaflet.admin import LeafletGeoAdmin
