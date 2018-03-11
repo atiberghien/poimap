932c266a6776682234b8709837bff33499ba6a51
Alban Tiberghien <alban.tiberghien@gmail.com>
2018-03-11 20:55:13 +0100

    Increase max

--- poimap/forms.py
+++ poimap/forms.py
@@ -6,7 +6,7 @@ from .models import POI
 class CustomItineraryForm(forms.Form):
     start_point = forms.ModelChoiceField(queryset=POI.objects.filter(type__slug='etape'), empty_label=None, initial=POI.objects.filter(type__slug='etape').first())
     end_point = forms.ModelChoiceField(queryset=POI.objects.filter(type__slug='etape'), empty_label=POI.objects.filter(type__slug='etape').last())
-    step = forms.IntegerField(label="Nombre de km/jour", widget=forms.NumberInput(attrs={'type':'range', 'min': 10, 'max': 30 , 'step':5}))
+    step = forms.IntegerField(label="Nombre de km/jour", widget=forms.NumberInput(attrs={'type':'range', 'min': 10, 'max': 40 , 'step':5}))
 
     class Media:
         css = {
