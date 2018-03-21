f9a581f1928084728acf7e9b6e318aef98c750f0
Alban Tiberghien <alban.tiberghien@gmail.com>
2018-03-21 15:56:15 +0100

    Filter according to starred field

--- poimap/forms.py
+++ poimap/forms.py
@@ -4,8 +4,8 @@ from django import forms
 from .models import POI
 
 class CustomItineraryForm(forms.Form):
-    start_point = forms.ModelChoiceField(queryset=POI.objects.filter(type__slug='etape'), empty_label=None, initial=POI.objects.filter(type__slug='etape').first())
-    end_point = forms.ModelChoiceField(queryset=POI.objects.filter(type__slug='etape'), empty_label=POI.objects.filter(type__slug='etape').last())
+    start_point = forms.ModelChoiceField(queryset=POI.objects.filter(starred=True), empty_label=None, initial=POI.objects.filter(starred=True).first())
+    end_point = forms.ModelChoiceField(queryset=POI.objects.filter(starred=True), empty_label=POI.objects.filter(starred=True).last())
     step = forms.IntegerField(label="Nombre de km/jour", widget=forms.NumberInput(attrs={'type':'range', 'min': 10, 'max': 40 , 'step':5}))
 
     class Media:
