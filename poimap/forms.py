a7a3ca4c0dd059df2b4a00d9ba04742ccfe4783f
Alban Tiberghien <alban.tiberghien@gmail.com>
2018-03-25 09:34:03 +0200

    Add label name

--- poimap/forms.py
+++ poimap/forms.py
@@ -4,8 +4,8 @@ from django import forms
 from .models import POI
 
 class CustomItineraryForm(forms.Form):
-    start_point = forms.ModelChoiceField(queryset=POI.objects.filter(starred=True), empty_label=None, initial=POI.objects.filter(starred=True).first())
-    end_point = forms.ModelChoiceField(queryset=POI.objects.filter(starred=True), empty_label=POI.objects.filter(starred=True).last())
+    start_point = forms.ModelChoiceField(label="Point de départ",queryset=POI.objects.filter(starred=True), empty_label=None, initial=POI.objects.filter(starred=True).first())
+    end_point = forms.ModelChoiceField(label="Point d'arrivée", queryset=POI.objects.filter(starred=True), empty_label=POI.objects.filter(starred=True).last())
     step = forms.IntegerField(label="Nombre de km/jour", widget=forms.NumberInput(attrs={'type':'range', 'min': 10, 'max': 40 , 'step':5}))
 
     class Media:
