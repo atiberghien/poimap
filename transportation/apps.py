9de6aedd36c8c239ce244f432218ae916164c080
Alban Tiberghien <alban.tiberghien@gmail.com>
2018-03-31 11:44:40 +0200

    Remove appname

--- transportation/apps.py
+++ transportation/apps.py
@@ -3,5 +3,5 @@ from __future__ import unicode_literals
 from django.apps import AppConfig
 
 
-class TumPagesConfig(AppConfig):
-    name = 'tum_pages'
+class TransportationConfig(AppConfig):
+    name = 'transportation'
