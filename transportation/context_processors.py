fb5d234bfbc3b145d90ee4f9728d7c5c11dd9f94
Alban Tiberghien <alban.tiberghien@gmail.com>
2018-06-12 10:14:53 +0200

    Fix context_processor

--- transportation/context_processors.py
+++ transportation/context_processors.py
@@ -3,10 +3,13 @@ from .models import Line
 from .forms import SearchServiceForm
 
 def transportation(request):
-    context = {}
+    context = {
+        "area" : Area.objects.first(),
+        "lines" : Line.objects.all(),
+        "search_form" : SearchServiceForm()
+    }
     if "travels" in request.session:
         context["cart_size"] = len(request.session["travels"])
     else:
         context["cart_size"] = 0
-    context["search_form"] = SearchServiceForm()
     return context
