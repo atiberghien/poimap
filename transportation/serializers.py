33e80a61f02471e3374ebaa1d0155ecb00031e0f
Alban Tiberghien <alban.tiberghien@gmail.com>
2019-02-25 16:08:40 +0100

    Add  Bus serialization

--- transportation/serializers.py
+++ transportation/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework.serializers import ModelSerializer, SerializerMethodField
 from poimap.serializers import POISerializer, PathSerializer
-from .models import Line, Stop, Route
+from .models import Line, Stop, Route, Service, Bus
 
 class StopSerializer(POISerializer):
     connection_count = SerializerMethodField()
@@ -24,9 +24,23 @@ class RouteSerialized(ModelSerializer):
         model = Route
         fields = ("id", "name", "slug", "direction", "path", "stops")
 
+class BusSerializer(ModelSerializer):
+    class Meta:
+        model = Bus
+        fields = "__all__"
+
 class LineSerializer(ModelSerializer):
     routes = RouteSerialized(many=True)
+    buses = SerializerMethodField()
+
+    def get_buses(self, obj):
+        buses = []
+        for service in Service.objects.filter(route__line=obj).order_by('route__line').distinct('route__line'):
+            for bus in service.bus_set.all():
+                if bus.license_plate:
+                    buses.append(bus) 
+        return BusSerializer(buses, many=True, read_only=True, context=self.context).data
 
     class Meta:
         model = Line
-        fields = ("id", "name", "slug", "connection_info" ,"routes")
+        fields = ("id", "name", "slug", "connection_info" ,"routes", 'buses')
