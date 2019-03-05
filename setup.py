90a183498d21ff39a8ed838f6dbf8af38d194d86
Alban Tiberghien <alban.tiberghien@gmail.com>
2019-03-05 15:12:56 +0100

    Replace leaflet.awesome-markers by beautifymarker

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='poimap',
-      version='2.7.0',
+      version='2.7.1',
       description='Manage POI on a map',
       url='https://github.com/atiberghien/poimap',
       author='Alban Tiberghien',
