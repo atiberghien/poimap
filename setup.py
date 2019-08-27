b04048c889208105b3c388f77127174b681c845c
Alban Tiberghien <alban.tiberghien@gmail.com>
2019-08-27 07:52:19 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='poimap',
-      version='3.0.0',
+      version='3.1.0',
       description='Manage POI on a map',
       url='https://github.com/atiberghien/poimap',
       author='Alban Tiberghien',
