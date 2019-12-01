b168833130b8f2cd35da45b00ce31fa4646f2c74
Alban Tiberghien <alban.tiberghien@gmail.com>
2019-12-01 16:31:10 +0100

    Fix undefined Jquery symbol

--- poimap/static/poimap/js/poi_admin_form.js
+++ poimap/static/poimap/js/poi_admin_form.js
@@ -1,52 +1,54 @@
-if (!$) {
-    $ = django.jQuery;
-}
+var django = {
+    "jQuery": jQuery.noConflict(true)
+};
+var jQuery = django.jQuery;
+var $=jQuery;
 
-var map = null;
-var field = null;
 
-$(window).on('map:init', function(e) {
-    map = e.originalEvent.detail.map;
-    map.on('map:loadfield', function (me) {
-        field = me.field
+(function($){
+    $(window).on('map:init', function(e) {
+        map = e.originalEvent.detail.map;
+        map.on('map:loadfield', function (me) {
+            field = me.field
+        });
     });
-});
+    
+    $(document).ready(function(){
 
-
-$(document).ready(function(){
-
-    $("#id_country").val("FR");
-    var address = $("#id_address");
-    var zipcode = $("#id_zipcode");
-    var city = $("#id_city");
-
-    var geoButton = $("<div class='field-box'><button type='button' class='default'>Géolocalisation à partir de l'adresse</button></div>").click(function(){
-        var country = $("#id_country option:selected");
-        if(country.val() && zipcode.val() && address.val()){
-            var addr = (address.val()+",+"+zipcode.val()+"+"+city.val()).replace(" ","+")
-            $.ajax({
-               dataType: "json",
-               url: "https://maps.googleapis.com/maps/api/geocode/json",
-               data: {
-                   address : addr,
-                   key : "AIzaSyAThNRSpWOhnA2EGSeshFqLolrTPQx1hjo"
-               },
-               success: function(data){
-                   if(data.status == "OK"){
-                       if(data.results.length > 0) {
-                           var cityResult = data.results[0]
-                           var lat = cityResult.geometry.location.lat;
-                           var lng = cityResult.geometry.location.lng;
-                           var marker = L.marker([lat, lng])
-                           field.store.save(marker);
-                           field.load();
-                           map.panTo(new L.LatLng(lat, lng));
-                           map.setZoom(17);
+        $("#id_country").val("FR");
+        var address = $("#id_address");
+        var zipcode = $("#id_zipcode");
+        var city = $("#id_city");
+    
+        var geoButton = $("<div class='field-box'><button type='button' class='default'>Géolocalisation à partir de l'adresse</button></div>").click(function(){
+            var country = $("#id_country option:selected");
+            if(country.val() && zipcode.val() && address.val()){
+                var addr = (address.val()+",+"+zipcode.val()+"+"+city.val()).replace(" ","+")
+                $.ajax({
+                   dataType: "json",
+                   url: "https://maps.googleapis.com/maps/api/geocode/json",
+                   data: {
+                       address : addr,
+                       key : "AIzaSyAThNRSpWOhnA2EGSeshFqLolrTPQx1hjo"
+                   },
+                   success: function(data){
+                       if(data.status == "OK"){
+                           if(data.results.length > 0) {
+                               var cityResult = data.results[0]
+                               var lat = cityResult.geometry.location.lat;
+                               var lng = cityResult.geometry.location.lng;
+                               var marker = L.marker([lat, lng])
+                               field.store.save(marker);
+                               field.load();
+                               map.panTo(new L.LatLng(lat, lng));
+                               map.setZoom(17);
+                           }
                        }
                    }
-               }
-           })
-       }
+               })
+           }
+        })
+        
+        $(".form-row.field-zipcode.field-city.field-country").append(geoButton);
     })
-    $(".form-row.field-zipcode.field-city.field-country").append(geoButton);
-});
+})(django.jQuery);
