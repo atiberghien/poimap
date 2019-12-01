9f5c4aa0846efb3f11a38d08291513ddb2546dd3
Alban Tiberghien <alban.tiberghien@gmail.com>
2019-12-01 16:32:50 +0100

    Increase pagiation in results

--- poimap/views.py
+++ poimap/views.py
@@ -96,7 +96,7 @@ class POIView(generics.RetrieveAPIView):
     serializer_class = POISerializer
 
 class StandardResultsSetPagination(PageNumberPagination):
-    page_size = 10
+    page_size = 100
     page_size_query_param = 'page_size'
     max_page_size = 1000
 
